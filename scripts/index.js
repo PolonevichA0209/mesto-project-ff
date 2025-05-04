64364ee04fc1ea9f38bb53de8cdbe5eb92ffb5d0
Andrey Polonevich <andrey.p0209@gmail.com>
2025-05-04 22:58:23 +0200

    ИСправил после замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,26 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCard(data, delCard) {
+function createCard(data) {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').src = data.link;
-    card.querySelector('.card__title').textContent = data.name;
-    card.querySelector('.card__delete-button').addEventListener('click', delCard);
+    const cardImg = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const deleteButton = card.querySelector('.card__delete-button');
+    cardImg.src = data.link;
+    cardImg.alt = data.name;
+    cardTitle.textContent = data.name;
+    deleteButton.addEventListener('click', () => deleteCard(card));
     return card;
 }
 
 // @todo: Функция удаления карточки
 
-function delCard(evt) {
-    const delTarget = evt.target.closest('.card');
-    delTarget.remove();
+function deleteCard(cardElement) {
+    cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-initialCards.map(el => {
-    placesList.append(createCard(el, delCard));
+initialCards.forEach(el => {
+    placesList.append(createCard(el, deleteCard));
 })
