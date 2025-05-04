0710f3fc34c4170039f760a4c1c4b725267f8c6c
Andrey Polonevich <andrey.p0209@gmail.com>
2025-05-04 21:31:35 +0200

    Первый вариант с готовым функционалом

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(data, delCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__image').src = data.link;
+    card.querySelector('.card__title').textContent = data.name;
+    card.querySelector('.card__delete-button').addEventListener('click', delCard);
+    return card;
+};
+
 // @todo: Функция удаления карточки
 
+function delCard(evt) {
+    const delTarget = evt.target.closest('.card');
+    delTarget.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+function render(data) {
+    data.map(el => {
+        placesList.append(createCard(el, delCard))
+    })
+}
+
+render(initialCards);
